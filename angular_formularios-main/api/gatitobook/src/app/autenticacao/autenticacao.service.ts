3800f49b129563b9cc23ac479e03fa95ca02b54d
viviane.luz <viviane.luz@jsl.com.br>
2022-12-26 15:31:56 -0300

    Criação component mensagem, login e home

--- angular_formularios-main/api/gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ angular_formularios-main/api/gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
-
 @Injectable({
   providedIn: 'root'
 })
--- angular_formularios-main/api/gatitobook/src/app/home/login/login.component.ts
+++ angular_formularios-main/api/gatitobook/src/app/home/login/login.component.ts
@@ -1,4 +1,6 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuario = '';
+  senha = '';
 
-  ngOnInit(): void {
+  constructor(private authService: AutenticacaoService, private router:Router) { }
+
+  ngOnInit(): void {}
+
+  login()  {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      () => {
+      this.router.navigate(['animais']);
+    },
+    (error) => {
+      alert("Usuário ou senha inválido")
+      console.log(error)
+    }
+    );  
   }
-
 }
